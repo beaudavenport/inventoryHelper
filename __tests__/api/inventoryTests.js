aa77e9d435ab3726b87788483e68c1367a12b5ab
Beau Davenport <beau.davenport@asynchrony.com>
2017-03-26 17:06:45 -0500

    Complete tests for inventory api

--- __tests__/api/inventoryTests.js
+++ __tests__/api/inventoryTests.js
@@ -4,7 +4,6 @@ import request from 'supertest';
 import app from '../../src/app';
 import db from '../../src/dbNew';
 import { getSessionStorageObject } from './testUtils';
-import { createToken } from '../../src/authorization';
 
 const COLLECTION_NAME = 'Potato';
 const COLLECTION_PASSWORD = 'Testw@rd1';
@@ -31,21 +30,146 @@ describe('inventory', () => {
 
   it('GET with type returns all items with specific type', () => {
     const type = 'Guacamole';
-    const record = {type, stuff: 'stuff'};
+    const record1 = {type, stuff: 'stuff'};
     const record2 = {type, thing: 'potato'};
     const record3 = {type: 'not right', item: 'who cares'};
 
-    db.get(COLLECTION_NAME).insert([record, record2, record3]);
+    db.get(COLLECTION_NAME).insert([record1, record2, record3])
+      .then(() => {
+        return request(app)
+          .get(`/inventory/${type}`)
+          .set('x-access-token', token)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            assert.strictEqual(response.body.length, 2);
+            assert.strictEqual(response.body[0].stuff, record1.stuff);
+            assert.strictEqual(response.body[1].thing, record2.thing);
+          })
+          .catch((err) => {
+            assert.fail('Error fetching inventory: ', err);
+          });
+      });
+  });
+
+  it('GET with id returns specific item by _id', () => {
+    const record1 = {stuff: 'stuff'};
+    const record2 = {stuff: 'bad stuff'};
+
+    db.get(COLLECTION_NAME).insert([record1, record2])
+      .then(() => {
+        return request(app)
+          .get(`/inventory/${record1._id}`)
+          .set('x-access-token', token)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            assert.strictEqual(response.body.stuff, record1.stuff);
+          })
+          .catch((err) => {
+            assert.fail('Error fetching inventory: ', err);
+          });
+      });
+  });
+
+  it('POST adds new inventory item and returns', () => {
+    const newItem = {
+      type: 'Cucumber',
+      thing: 'Great information'
+    };
+
     return request(app)
-      .get(`/inventory/${type}`)
+      .post('/inventory')
+      .send(newItem)
       .set('x-access-token', token)
       .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
       .expect(200)
       .then((response) => {
-        assert.strictEqual(response.body.length, 2);
-        assert.deepEqual(response.body[0].stuff, record.stuff);
-        assert.deepEqual(response.body[1].thing, record2.thing);
+        assert.strictEqual(response.body.type, newItem.type);
+        assert.strictEqual(response.body.thing, newItem.thing);
+        return response.body._id;
+      })
+      .then((itemId) => {
+        return db.get(COLLECTION_NAME).findOne({_id: itemId})
+          .then((result) => {
+            assert.strictEqual(result.type, newItem.type);
+            assert.strictEqual(result.thing, newItem.thing);
+          });
+      })
+      .catch((err) => {
+        assert.fail('Error fetching inventory: ', err);
+      });
+  });
+
+  it('PUT updates item and returns', () => {
+    const itemToUpdate = {
+      type: 'Cucumber',
+      thing: 'OLD information'
+    };
+    return db.get(COLLECTION_NAME).insert(itemToUpdate)
+      .then(() => {
+        return request(app)
+          .put(`/inventory/${itemToUpdate._id}`)
+          .send({thing: 'NEW information'})
+          .set('x-access-token', token)
+          .set('Accept', 'application/json')
+          .expect(200)
+          .then((response) => {
+            assert.strictEqual(response.body.thing, 'NEW information');
+            return response.body._id;
+          })
+          .then((itemId) => {
+            return db.get(COLLECTION_NAME).findOne({_id: itemId})
+              .then((result) => {
+                assert.strictEqual(result.thing, 'NEW information');
+              });
+          });
+      })
+      .catch((err) => {
+        assert.fail('Error fetching inventory: ', err);
+      });
+  });
+
+  it('DELETE with id deletes item by _id', () => {
+    const record = {stuff: 'stuff'};
+
+    return db.get(COLLECTION_NAME).insert(record)
+      .then(() => {
+        return request(app)
+          .delete(`/inventory/${record._id}`)
+          .set('x-access-token', token)
+          .set('Accept', 'application/json')
+          .expect('Content-Type', /json/)
+          .expect(200)
+          .then((response) => {
+            assert.equal(response.body._id, record._id);
+          })
+          .then(() => {
+            return db.get(COLLECTION_NAME).findOne({_id: record._id})
+              .then((result) => {
+                assert.strictEqual(result, null);
+              });
+          })
+          .catch((err) => {
+            assert.fail('Error fetching inventory: ', err);
+          });
+      });
+  });
+
+  it('PUT SYNC updates item and returns', () => {
+    return db.get(COLLECTION_NAME).insert({'date':'date'})
+      .then(() => {
+        return request(app)
+          .put('/inventory/sync/foo')
+          .set('x-access-token', token)
+          .set('Accept', 'application/json')
+          .expect(200)
+          .then((response) => {
+            assert.ok(response.body.lastSync);
+            assert.notEqual(response.body.lastSync, 'never');
+          });
       })
       .catch((err) => {
         assert.fail('Error fetching inventory: ', err);
--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -2,13 +2,11 @@
 * Inventory Route provides the RESTful API for database access.
 * It is protected by a JWT authentication middleware file.
 */
+import express from 'express';
+import jwtAuth from '../jwtauth.js';
+import moment from 'moment';
 
-var express = require('express');
-var jwtAuth = require('../jwtauth.js');
-var ObjectID = require('mongodb').ObjectID;
-var moment = require('moment');
-
-var router = express.Router();
+const router = express.Router();
 
 //any API call must pass JWT authentication
 router.all('/*', jwtAuth);
@@ -30,76 +28,85 @@ router.get('/:type', (req, res) => {
 });
 
 //get requests with an id specified return a single result by ObjectID
-router.get('/:id', function(req, res) {
-    var itemID = new ObjectID(req.params.id);
-    var db = req.db;
-    var requestedCollection = req.inventoryName;
-    db.collection(requestedCollection).findOne({_id: itemID}, function(err, result) {
-        if(!err) {
-            res.json(result);
-        } else {
-            res.end('An error occured.');
-        }
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const db = req.dbNew;
+  const inventoryName = req.inventoryName;
+
+  db.get(inventoryName).findOne({_id: id})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.status(404).json({error: err});
     });
 });
 
 //post requests have a 'category' field specified client-side for later retrieval by category
-router.post('/', function(req, res) {
-    var newItem = req.body;
-    var db = req.db;
-    var requestedCollection = req.inventoryName;
-    db.collection(requestedCollection).insert(newItem, {safe: true}, function(error, result, status) {
-        if (!error) {
-            res.send(newItem);
-        } else {
-            res.end('An error occured.');
-        }
+router.post('/', (req, res) => {
+  const newItem = req.body;
+  const db = req.dbNew;
+  const inventoryName = req.inventoryName;
+
+  db.get(inventoryName).insert(newItem, {safe: true})
+    .then(result => {
+      res.json(result);
+    })
+    .catch(err => {
+      res.status(404).json({error: err});
     });
 });
 
 //put requests update a single result by ObjectID
-router.put('/:id', function(req, res) {
-    var itemUpdate = req.body;
-    //remove '_id' field from request body to prevent conflict during update
-    var itemID = ObjectID(req.params.id);
-    delete itemUpdate._id;
-    var db = req.db;
-    var requestedCollection = req.inventoryName;
-    db.collection(requestedCollection).update({_id: itemID}, itemUpdate, {safe: true}, function(error, result, status) {
-        if (!error) {
-            res.send(itemUpdate);
-        } else {
-            res.end('An error occured.');
-        }
+router.put('/:id', (req, res) => {
+  const update = req.body;
+  const id = req.params.id;
+  const db = req.dbNew;
+  const inventoryName = req.inventoryName;
+  const inventory = db.get(inventoryName);
+
+  inventory.update({_id: id}, update)
+    .then(() => {
+      return inventory.findOne({_id: id})
+        .then(result => {
+          res.json(result);
+        });
+    })
+    .catch(err => {
+      res.status(404).json({error: err});
     });
 });
 
 //delete single result by ObjectID
-router.delete('/:id', function(req, res) {
-    var itemId = req.params.id;
-    var db = req.db;
-    var requestedCollection = req.inventoryName;
-    db.collection(requestedCollection).removeById(itemId, {safe: true}, function(error, result, status) {
-        if (!error) {
-            res.send(itemId);
-        } else {
-            res.end('An error occured.');
-        }
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  const db = req.dbNew;
+  const inventoryName = req.inventoryName;
+
+  db.collection(inventoryName).remove({_id: id})
+    .then(() => {
+      res.json({_id: id});
+    })
+    .catch(err => {
+      res.status(404).json({error: err});
     });
 });
 
 //update time of last 'Sync to database' event
-router.put('/sync/:id', function(req, res) {
-    var db = req.db;
-    var requestedCollection = req.inventoryName;
-    var lastSync = moment.utc().toJSON();
-    console.log(lastSync);
-    db.collection(requestedCollection).update({'date': 'date'}, { $set: {'lastSync': lastSync}}, {safe: true}, function(error, result, status) {
-        if (!error) {
-            res.send(JSON.stringify(lastSync));
-        } else {
-            res.end('An error occured.');
-        }
+router.put('/sync/:id', (req, res) => {
+  const db = req.dbNew;
+  const inventoryName = req.inventoryName;
+  const inventory = db.get(inventoryName);
+
+  inventory.update({'date': 'date'}, {'date':'date', 'lastSync': moment.utc().toJSON()}, {safe: true})
+    .then(() => {
+      return inventory.findOne({'date': 'date'})
+        .then(result => {
+          res.json(result);
+        });
+    })
+    .catch(err => {
+      res.status(404).json({error: err});
     });
 });
 
