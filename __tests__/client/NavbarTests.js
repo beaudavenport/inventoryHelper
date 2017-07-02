9155f74df56156ef9983e34e3fb5986ecde2841c
Beau Davenport <beau.davenport@asynchrony.com>
2017-07-02 10:56:07 -0500

    Display title in navbar from metadata record if logged in

--- __tests__/client/NavbarTests.js
+++ __tests__/client/NavbarTests.js
@@ -2,24 +2,35 @@ import React from 'react';
 import mocha from 'mocha';
 import assert from 'assert';
 import sinon from 'sinon';
+import configureStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
 import Navbar from '../../src/client/Navbar';
 
 describe('Navbar', () => {
-  it('renders default title if no current collection chosen', () => {
-    const wrapper = shallow(<Navbar />);
+  it('renders default title if not logged in', () => {
+    const mockStore = configureStore()({metadata: {}});
+    const wrapper = shallow(<Navbar store={mockStore} />);
 
-    const collectionName = wrapper.find('.collection-name');
+    const collectionName = wrapper.dive().find('.collection-name');
     assert.strictEqual(collectionName.text(), 'New Inventory');
   });
 
-  it('displays login/create bar when selected', () => {
-    const wrapper = shallow(<Navbar />);
+  it('renders collection name if logged in', () => {
+    const mockStore = configureStore()({metadata: {collectionName: 'GreatBigPotatoes'}});
+    const wrapper = shallow(<Navbar store={mockStore} />);
 
-    const loginButton = wrapper.find('.login-toggle');
+    const collectionName = wrapper.dive().find('.collection-name');
+    assert.strictEqual(collectionName.text(), 'GreatBigPotatoes');
+  });
+
+  it('displays login/create bar when selected', () => {
+    const mockStore = configureStore()({metadata: {}});
+    const wrapper = shallow(<Navbar store={mockStore}/>);
+
+    const loginButton = wrapper.dive().find('.login-toggle');
     loginButton.simulate('click');
 
-    const loginBar = wrapper.find('.login-bar');
+    const loginBar = wrapper.dive().find('.login-bar');
     assert(loginBar);
   });
 });
--- src/client/Navbar.js
+++ src/client/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 import SaveButton from './SaveButton';
 
@@ -12,7 +14,7 @@ class Navbar extends React.Component {
 
   render() {
     const loginBarToggle = () => this.setState({loginBarOpen: !this.state.loginBarOpen});
-
+    const collectionName = this.props.metadata.collectionName || 'New Inventory';
     let loginBar = null;
     if(this.state.loginBarOpen) {
       loginBar = <div className="login-bar">Great Scott!</div>;
@@ -22,7 +24,7 @@ class Navbar extends React.Component {
       <div>
         <nav className="navbar navbar-inverse bg-inverse">
           <Link className="navbar-brand" to="/">
-            Inventory Helper: <span className="collection-name">New Inventory</span>
+            Inventory Helper: <span className="collection-name">{collectionName}</span>
           </Link>
           <ul className="navbar-nav">
             <li className="nav-item"><Link to="/containers" className="nav-link">Containers</Link></li>
@@ -36,4 +38,12 @@ class Navbar extends React.Component {
   }
 }
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    metadata: state.metadata
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
